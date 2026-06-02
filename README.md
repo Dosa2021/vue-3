4dad77fad8c34cfc7bb638448213b257c34021ac
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-03 07:39:56 +0900

    プロジェクト作成

---
+++ README.md
@@ -0,0 +1,11 @@
+npm create vue@latest
+cd vue-project
+npm install
+npm run dev
+
+【参考】
+・Vue.js入門(Vue.jsの初心者は必ず読め‼︎)
+https://qiita.com/Hashimoto-Noriaki/items/ea60d5932f13a9cd5707
+
+・拡張
+https://qiita.com/tetusan/items/81b1f863f28731dcb19c
---
+++ vue-project/src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header>
+    <p>
+      <img src="/mei.webp" />
+    </p>
+    <h1>山田太郎</h1>
+    <p>Webデザイナー見習いです</p>
+  </header>
+
+  <main>
+    <h2>WORKS</h2>
+    <section>
+      <img src="" alt="すごい勇者ゲーム" width="400" height="240" />
+      <h3>すごい勇者ゲーム</h3>
+      <p>
+        すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。
+      </p>
+    </section>
+    <section>
+      <img src="" alt="すごい宝物ゲーム" width="400" height="240" />
+      <h3>すごい宝物ゲーム</h3>
+      <p>
+        すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。
+      </p>
+    </section>
+    <section>
+      <img src="" alt="すごいカードゲーム" width="400" height="240" />
+      <h3>すごいカードゲーム</h3>
+      <p>
+        すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。
+      </p>
+    </section>
+  </main>
+  <footer>
+    <small>(c) dotinstall.com</small>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
