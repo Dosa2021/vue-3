0aa6e5370744c3c57a176d3bcd3cc45d58820670
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-06-08 11:22:28 +0900

    v-on

--- vue-project/src/components/Header.vue
+++ vue-project/src/components/Header.vue
@@ -1,14 +1,36 @@
 <script setup>
+import { ref } from 'vue';
+
 const intro = 'フロントエンドエンジニア見習いです'; 
+const imgUrl = '/mei.webp';
+const count = ref(0)
+
+function increment() {
+  // count++
+  count.value++
+}
 </script>
 
 <template>
   <header>
     <p>
-      <img src="/mei.webp" />
+      <!-- <img src="/mei.webp" /> -->
+
+      <!-- Note: v-bind -->
+      <!-- <img v-bind:src="imgUrl" /> -->
+      <img :src="imgUrl" />
     </p>
     <h1>山田太郎</h1>
+
+    <!-- Note: v-text -->
     <p v-text="intro"></p>
+    <!-- <p>{{ intro }}</p>
+    <p>{{ intro.length }}</p> -->
+
+    <!-- Note: v-on -->
+    <!-- <button v-on:click="count++">いいね！（{{ count }}）</button> -->
+    <!-- <button @click="count++">いいね！（{{ count }}）</button> -->
+    <button @click="increment">いいね！（{{ count }}）</button>
   </header>
 </template>
 
