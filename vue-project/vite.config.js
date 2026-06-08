e8c22d72f5174eaa447b12ac416d98d6178f5591
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-08 10:01:28 +0900

    Dockerで起動

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
